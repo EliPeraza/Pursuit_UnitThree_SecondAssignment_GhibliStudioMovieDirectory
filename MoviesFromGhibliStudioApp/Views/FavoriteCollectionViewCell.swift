b20cf306507a2712a57f52cae2ce53206016f8b1
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-01-08 01:33:13 -0500

    MVP

---
+++ MoviesFromGhibliStudioApp/Views/FavoriteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  MoviesFromGhibliStudioApp
+//
+//  Created by Elizabeth Peraza  on 1/8/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+}
